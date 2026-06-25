dbfbd94dce032ea2a9d22a58b189c2c51cf4e675
Oleh-323 <oleh.vlas.323@gmail.com>
2026-06-26 00:42:54 +0300

    add section with info for: Contact Us/Support/Connect & Learn

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -43,6 +43,27 @@ const sectionAboutMenu: SectionMenuType[]= [
     {title: "Privacy Policy", description: "Lorem ipsum dolor sit amet consectetur. Felis ullamcorper nunc vel tincidunt ultrices.", href: "#privacy-policy"}
 ];
 
+const sectionContactUsMenu: SectionMenuType[]= [
+    {title: "Contact Us", description: "Lorem ipsum dolor sit amet consectetur. Felis ullamcorper nunc vel tincidunt ultrices.", href: "#contact-us"},
+    {title: "Your Community IT Support in Charlottesville", description: "Lorem ipsum dolor sit amet consectetur. Felis ullamcorper nunc vel tincidunt ultrices.", href: "#it-support"},
+    {title: "Your Local IT Consulting Company in Richmond", description: "Lorem ipsum dolor sit amet consectetur. Felis ullamcorper nunc vel tincidunt ultrices.", href: "#local-it"},
+    {title: "Complete Managed IT Services for Washington", description: "Lorem ipsum dolor sit amet consectetur. Felis ullamcorper nunc vel tincidunt ultrices.", href: "#complete-managed-it"}
+];
+
+const sectionSupportMenu: SectionMenuType[]= [
+    {title: "I have a support PIN", description: "Lorem ipsum dolor sit amet consectetur. Felis ullamcorper nunc vel tincidunt ultrices.", href: "#i-have-a-support-pin"},
+    {title: "Submit a Request", description: "Lorem ipsum dolor sit amet consectetur. Felis ullamcorper nunc vel tincidunt ultrices.", href: "#submit-a-request"}
+];
+
+const sectionConnectLearnMenu: SectionMenuType[]= [
+    {title: "Cases", description: "Lorem ipsum dolor sit amet consectetur. Felis ullamcorper nunc vel tincidunt ultrices.", href: "#cases"},
+    {title: "Soligent", description: "Lorem ipsum dolor sit amet consectetur. Felis ullamcorper nunc vel tincidunt ultrices.", href: "#soligent"},
+    {title: "Weekly Webinar", description: "Lorem ipsum dolor sit amet consectetur. Felis ullamcorper nunc vel tincidunt ultrices.", href: "#weekly-webinar"},
+    {title: "Follow Us On Social Media", description: "Lorem ipsum dolor sit amet consectetur. Felis ullamcorper nunc vel tincidunt ultrices.", href: "#follow-us-on-social-media"},
+    {title: "Blog", description: "Lorem ipsum dolor sit amet consectetur. Felis ullamcorper nunc vel tincidunt ultrices.", href: "#blog"},
+    {title: "23andMe’s $30 Million Settlement: What It Means for Genetic Data Privacy", description: "Lorem ipsum dolor sit amet consectetur. Felis ullamcorper nunc vel tincidunt ultrices.", href: "#what-it-means"},
+    {title: "Newsletter", description: "Lorem ipsum dolor sit amet consectetur. Felis ullamcorper nunc vel tincidunt ultrices.", href: "#newsletter"}
+];
 
 function clickMessage(){
     const message= "send me a message on my telegram @ContuctNumber"
@@ -52,6 +73,9 @@ function clickMessage(){
 function Header(){
     const [isServicesOpen, setIsServicesOpen] = useState(false);
     const [isAboutOpen, setIsAboutOpen] = useState(false);
+    const [isContactUsOpen, setIsContactUsOpen] = useState(false);
+    const [isSupportOpen, setIsSupportOpen] = useState(false);
+    const [isConnectLearnOpen, setIsConnectLearnOpen] = useState(false);
 
     function handleMenuComponent(menuKey: string) {
         if (menuKey == "Services") {
@@ -64,9 +88,30 @@ function Header(){
         } else {
             setIsAboutOpen(false);
         }
+        if (menuKey == "Contact us"){
+            setIsContactUsOpen(true);
+        } else {
+            setIsContactUsOpen(false);
+        }
+        if (menuKey == "Support"){
+            setIsSupportOpen(true);
+        } else {
+            setIsSupportOpen(false);
+        }
+        if (menuKey == "Connect & Learn"){
+            setIsConnectLearnOpen(true);
+        } else {
+            setIsConnectLearnOpen(false);
+        }
     }
 
-
+    function closeAllMenus() {
+        setIsServicesOpen(false);
+        setIsAboutOpen(false);
+        setIsContactUsOpen(false);
+        setIsSupportOpen(false);
+        setIsConnectLearnOpen(false);
+    }
 
     return (
         <>
@@ -82,7 +127,7 @@ function Header(){
 
                     <button onClick={clickMessage}>Contact us <img src={arrowUp.src} /></button>
                 </header>
-                <section style={{ "display": isServicesOpen ? "grid": "none" }} className={style.particle} >
+                <section style={{ "display": isServicesOpen ? "grid": "none" }} onMouseLeave={closeAllMenus} className={style.particle} >
                     {sectionServicesMenu.map((index)=>
                         <a className={style.part} title={index.title} href={index.href}>
                             <h1>{index.title}</h1>
@@ -90,8 +135,32 @@ function Header(){
                         </a>
                     )}
                 </section>
-                <section style={{ "display": isAboutOpen ? "grid": "none" }} className={style.particle} >
+                <section style={{ "display": isAboutOpen ? "grid": "none" }} onMouseLeave={closeAllMenus} className={style.particle} >
                     {sectionAboutMenu.map((index)=>
+                        <a className={style.part} title={index.title} href={index.href}>
+                            <h1>{index.title}</h1>
+                            <span className={style.description}>{index.description}</span>
+                        </a>
+                    )}
+                </section>
+                <section style={{ "display": isContactUsOpen ? "grid": "none" }} onMouseLeave={closeAllMenus} className={style.particle} >
+                    {sectionContactUsMenu.map((index)=>
+                        <a className={style.part} title={index.title} href={index.href}>
+                            <h1>{index.title}</h1>
+                            <span className={style.description}>{index.description}</span>
+                        </a>
+                    )}
+                </section>
+                <section style={{ "display": isSupportOpen ? "grid": "none" }} onMouseLeave={closeAllMenus} className={style.particle} >
+                    {sectionSupportMenu.map((index)=>
+                        <a className={style.part} title={index.title} href={index.href}>
+                            <h1>{index.title}</h1>
+                            <span className={style.description}>{index.description}</span>
+                        </a>
+                    )}
+                </section>
+                <section style={{ "display": isConnectLearnOpen ? "grid": "none" }} onMouseLeave={closeAllMenus} className={style.particle} >
+                    {sectionConnectLearnMenu.map((index)=>
                         <a className={style.part} title={index.title} href={index.href}>
                             <h1>{index.title}</h1>
                             <span className={style.description}>{index.description}</span>
